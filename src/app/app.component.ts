f0124c6dfe6cc819079dd31e8289ed3113d0ae92
Fihoca06 <Fihoca06@SINENSIA.EDU>
2019-12-16 17:15:25 +0100

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'testeo';
+
+
+export class AppComponent implements OnInit{
+
+  //iban: FormGroup;
+
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  
+  
+    valor1=null;
+    valor2=null;
+    resultado=null;
+    opcion1=false;
+    opcion2=false;
+    opcion3=false;
+    opcion4=false;
+ 
+  constructor(private formBuilder: FormBuilder) {
+    this.firstFormGroup = this.formBuilder.group({
+      iban:['',Validators.required],
+      DNI:['',Validators.required],
+      name:['',Validators.required]
+  });
+}
+ 
+  ngOnInit() {
+  
+    this.firstFormGroup = this.formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this.formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+
+
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatInputModule} from '@angular/material/input'; 
+import {MatButtonModule} from '@angular/material/button';
+
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+ 
+import { CUSTOM_ELEMENTS_SCHEMA }      from '@angular/core'; 
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatStepperModule, MatInputModule, MatButtonModule,MatButtonToggleModule,
+    MatCheckboxModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule
+
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
